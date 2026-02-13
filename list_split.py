28db9eba88aeb818ea6ee7ddcb8930f034690901
Saujanya Srivastava <154549622+icequeen3104@users.noreply.github.com>
2026-02-13 13:16:10 +0530

    Update list_split.py

--- list_split.py
+++ list_split.py
@@ -1,10 +1,7 @@
-#names = " alex, john, maria, anna, bob "
-#new_names = names.split(",")
-#new_names = names.split("-")#this will not separate into a set of values in list, it will only show one value in list
-#print(new_names)
-
-names = [" alex; john; maria; anna; bob "]
-names= [name.strip() for name in names] #remove the extra spaces in the list in the last and the beginning
-print(names)
+names = " alex; john; maria; anna; bob "
+new_names = names.split(";")
+#new_names = names.split("-")#this will separate the values wherever it will find "-"
+new_names = [name.strip() for name in new_names] #this removes extra spaces
+print(new_names)
 
 
